41f11c5eb608aa57150f6fe1619b1a8e38dfd199
A. Mert Bozkurt <a.mertbozkurt@gmail.com>
2022-01-31 13:42:18 +0100

    added two py files

---
+++ script.py
@@ -0,0 +1,8 @@
+print("Hello world")
+
+from test import circumference, surface_area
+
+print(circumference(4))
+print(surface_area(4))
+
+
---
+++ test.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+import numpy as np
+
+
+def circumference(r):
+    return 2.0*np.pi*r
+
+
+def surface_area(r):
+    return np.pi*r**2
