60385e204995d03d4639a8d3c270141f4e41c819
Sebastian <sebastian.miles@live.de>
2022-01-31 14:41:37 +0100

    Added working branch for Sebastian and its initial files

---
+++ PicGenerator.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+import scipy as sci
+import numba as nb
+from numba import jit,njit
+import matplotlib.pyplot as plt
+
+
+
+""" 
+This file is meant to generate normalized figures based on the Multibrot sequences.
+
+"""
